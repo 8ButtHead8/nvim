2aa56ae943b4611d79df560c4cbe3f50ecefac74
8ButtHead8 <butthead.prod@gmail.com>
2024-01-25 00:22:48 +0400

    SCSS autocomplete

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,2 +1,16 @@
 vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
 vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+
+vim.api.nvim_create_user_command('ScssSource', function()
+	vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+		pattern = { "*.scss" },
+		command = "silent !sass --style=compressed %:p %:s?_??:r.css",
+	})
+end, {})
+
+vim.api.nvim_create_user_command('ScssNosource', function()
+	vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+		pattern = { "*.scss" },
+		command = "silent !sass --no-source-map --style=compressed %:p %:s?_??:r.css",
+	})
+end, {})
