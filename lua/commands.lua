7f60c6539c8f4e15a3ea89369474d58fc6eda5dd
8ButtHead8 <butthead.prod@gmail.com>
2024-01-25 02:11:23 +0400

    autoprefixer

--- lua/commands.lua
+++ lua/commands.lua
@@ -2,19 +2,24 @@ vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
 vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
 
 vim.api.nvim_create_user_command('ScssSource', function()
-	local scss_autocomplete_group = vim.api.nvim_create_augroup('scss_autocomplete', { clear = true })
-	vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-		group = scss_autocomplete_group,
-		pattern = { "*.scss" },
-		command = "silent !sass --style=compressed %:p %:s?_??:r.css",
-	})
+	setSCSSCommand(true)
 end, {})
 
 vim.api.nvim_create_user_command('ScssNosource', function()
+	setSCSSCommand(false)
+end, {})
+
+function setSCSSCommand(sourceMap)
 	local scss_autocomplete_group = vim.api.nvim_create_augroup('scss_autocomplete', { clear = true })
 	vim.api.nvim_create_autocmd({ "BufWritePost" }, {
 		group = scss_autocomplete_group,
 		pattern = { "*.scss" },
-		command = "silent !sass --no-source-map --style=compressed %:p %:s?_??:r.css",
+		command = "!sass " ..
+				(sourceMap and "--source-map " or "--no-source-map ") .. "--style=compressed %:p %:s?_??:r.css",
 	})
-end, {})
+	vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+		group = scss_autocomplete_group,
+		pattern = { "*.scss" },
+		command = "!postcss %:r.css --use autoprefixer -o %:r.css",
+	})
+end
